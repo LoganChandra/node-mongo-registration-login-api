9ec8435a95d33b8350830e714c7f6fbe2590bcef
LoganChandra <45206354+LoganChandra@users.noreply.github.com>
2021-06-15 12:47:55 +0800

    added movie schema

---
+++ movies/movie.controller.js
@@ -0,0 +1,44 @@
+﻿const express = require('express');
+const router = express.Router();
+const movieService = require('./movie.service');
+
+// routes
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+
+module.exports = router;
+
+
+function register(req, res, next) {
+    movieService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    movieService.getAll()
+        .then(movies => res.json(movies))
+        .catch(err => next(err));
+}
+
+
+function getById(req, res, next) {
+    movieService.getById(req.params.id)
+        .then(movie => movie ? res.json(movie) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    movieService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    movieService.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
---
+++ movies/movie.service.js
@@ -0,0 +1,57 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const Movie = db.Movie;
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+async function getAll() {
+    return await Movie.find();
+}
+
+async function getById(id) {
+    return await Movie.findById(id);
+}
+
+async function create(movieParam) {
+    
+    if (await Movie.findOne({ moviename: movieParam.moviename })) {
+        throw 'Username "' + movieParam.moviename + '" is already taken';
+    }
+
+    const movie = new Movie(movieParam);
+
+    if (movieParam.password) {
+        movie.hash = bcrypt.hashSync(movieParam.password, 10);
+    }
+
+    await movie.save();
+}
+
+async function update(id, movieParam) {
+    const movie = await Movie.findById(id);
+
+    if (!movie) throw 'Movie not found';
+    if (movie.moviename !== movieParam.moviename && await Movie.findOne({ moviename: movieParam.moviename })) {
+        throw 'Username "' + movieParam.moviename + '" is already taken';
+    }
+
+    if (movieParam.password) {
+        movieParam.hash = bcrypt.hashSync(movieParam.password, 10);
+    }
+
+    Object.assign(movie, movieParam);
+
+    await movie.save();
+}
+
+async function _delete(id) {
+    await Movie.findByIdAndRemove(id);
+}
